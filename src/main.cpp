c3dbf334c04da56d2b448b4913b2ba0a19c9da13
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2024-01-07 04:02:14 -0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define setPin 2
+
+void setup()
+{
+    delay(1000);
+
+    Serial5.begin(9600); // antenna
+    Serial.begin(9600);
+    
+    delay(1000);
+
+    Serial.print("testing");
+
+    pinMode(setPin, OUTPUT);
+    digitalWrite(setPin, LOW);
+    delay(1000);
+
+    Serial5.println("AT+C021");
+    delay(1000);
+}
+
+void loop() {
+    char receivedChar = Serial5.read();
+    Serial.println(receivedChar);
+    delay(100);
+
+}
